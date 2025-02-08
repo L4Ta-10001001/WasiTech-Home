33612dbd44c0faa12b325486d2bef7baa500f3f3
d3rickArch <lspotify3022l@gmail.com>
2025-02-08 16:04:45 -0500

    Creacion del proyecto

---
+++ src/main/java/com/uce/wasitech/WasitechApplication.java
@@ -0,0 +1,13 @@
+package com.uce.wasitech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasitechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasitechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uce/wasitech/WasitechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uce.wasitech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasitechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
